347c9646cf38fda474d53c9fde5121bb6d05ce91
SvyatoslavStrakhnitsky <sviatoyyy@gmail.com>
2023-05-15 09:54:00 +0300

    deploy

--- src/process/NewProjectForm/model/slice/projectFormSlice.ts
+++ src/process/NewProjectForm/model/slice/projectFormSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import e from 'express';
 import { ProjectFormSchema } from '../types/ProjectFormSchema';
 
 const initialState: ProjectFormSchema = {
